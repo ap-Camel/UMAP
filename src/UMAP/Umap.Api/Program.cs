8b706b1d26d8a804c79315fdc009ad465587c82f
ayman <ayman.abdwlla@gmail.com>
2025-08-23 21:42:43 +0200

    fixing project setup and adding tesseract trained data.

--- src/UMAP/Umap.Api/Program.cs
+++ src/UMAP/Umap.Api/Program.cs
@@ -1,4 +1,4 @@
-
+using System;
 using Umap.Api.Services;
 using Umap.Api.Services.impl;
 
@@ -6,36 +6,44 @@ namespace Umap.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddSingleton<IPaddleOcr, PaddleOcr>();
-            builder.Services.AddSingleton<IScreenCaptureService, ScreenCaptureService>();
-            builder.Services.AddSingleton<IScreenCropService, ScreenCropService>();
-            builder.Services.AddSingleton<IUmaMusumeWindowBringToFrontService, UmaMusumeWindowBringToFrontService>();
-            builder.Services.AddSingleton<IWindowBoundsService, WindowBoundsService>();
-            builder.Services.AddSingleton<IWindowService, WindowService>();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
 
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-           
-            app.MapOpenApi();
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+                // Add services to the container.
+                builder.Services.AddSingleton<IPaddleOcr, PaddleOcr>();
+                builder.Services.AddSingleton<IScreenCaptureService, ScreenCaptureService>();
+                builder.Services.AddSingleton<IScreenCropService, ScreenCropService>();
+                builder.Services.AddSingleton<IUmaMusumeWindowBringToFrontService, UmaMusumeWindowBringToFrontService>();
+                builder.Services.AddSingleton<IWindowBoundsService, WindowBoundsService>();
+                builder.Services.AddSingleton<IWindowService, WindowService>();
 
 
-            app.MapControllers();
+                builder.Services.AddControllers();
+                // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+                //builder.Services.AddOpenApi();
 
-            app.Run();
+                var app = builder.Build();
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                app.UseRouting();
+
+                app.MapDefaultControllerRoute();
+
+                await app.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
         }
     }
 }
